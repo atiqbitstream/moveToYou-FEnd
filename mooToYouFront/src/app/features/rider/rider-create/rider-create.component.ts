7f1881ca434167bb77f80447ef1f31f96f6f7aae
atiqBytes <atiqbytes@gmail.com>
2024-11-18 21:15:52 +0500

    created 'riders' module and its nested components with lazy loading , just initialized simply

---
+++ mooToYouFront/src/app/features/rider/rider-create/rider-create.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-rider-create',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './rider-create.component.html',
+  styleUrl: './rider-create.component.css'
+})
+export class RiderCreateComponent {
+
+}
---
+++ mooToYouFront/src/app/features/rider/rider-list/rider-list.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-rider-list',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './rider-list.component.html',
+  styleUrl: './rider-list.component.css'
+})
+export class RiderListComponent {
+
+}
