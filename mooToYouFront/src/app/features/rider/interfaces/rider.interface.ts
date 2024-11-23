ca2be96d3a1744f0bcde27f476ef8177e45143d4
atiqBytes <atiqbytes@gmail.com>
2024-11-23 20:40:14 +0500

    listing 'riders' and single 'rider'

---
+++ mooToYouFront/src/app/features/rider/interfaces/rider.interface.ts
@@ -0,0 +1,13 @@
+export interface Rider {
+    id: number;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    address: string;
+    sector: string;
+    street: string;
+    cnicNumber: string;
+    createdAt: string;
+    updatedAt: string;
+    isDeleted: boolean;
+  }
