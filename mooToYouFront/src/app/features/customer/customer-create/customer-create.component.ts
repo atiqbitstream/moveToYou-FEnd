310a55bfa1f1d3d3a735d9f509dcd99cc484a712
atiqbitstream <atiqbitstream@gmail.com>
2024-11-29 18:09:50 +0500

    created a 'customer' module component  to do customer related operations

---
+++ mooToYouFront/src/app/features/customer/customer-create/customer-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-create',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-create.component.html',
+  styleUrl: './customer-create.component.css'
+})
+export class CustomerCreateComponent {
+
+}
