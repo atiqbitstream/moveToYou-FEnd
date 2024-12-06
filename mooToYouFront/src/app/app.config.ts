0a9b02d62e432350d3464b475c6eee30679f1ab5
atiqbitstream <atiq.bitstream@gmail.com>
2024-12-06 18:55:47 +0500

    minors

--- mooToYouFront/src/app/app.config.ts
+++ mooToYouFront/src/app/app.config.ts
@@ -4,7 +4,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { authInterceptor } from './features/login/Interceptors/auth.interceptor';
+import { authInterceptor } from './features/shared/Interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
