5d96a460fe7884875fdaa23db00b7e65f068341e
atiqbitstream <atiq.bitstream@gmail.com>
2024-12-15 10:55:28 +0500

    Rider main page gui done

--- mooToYouFront/src/app/features/rider/rider.component.ts
+++ mooToYouFront/src/app/features/rider/rider.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-rider',
   standalone: true,
-  imports: [RouterOutlet,RouterModule],
+  imports: [RouterLink,  MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule],
   templateUrl: './rider.component.html',
   styleUrl: './rider.component.css'
 })
 export class RiderComponent {
+
+  
 
 }
