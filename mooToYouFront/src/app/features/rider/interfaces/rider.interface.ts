f6dc9af91911158e094065c79ed6c54e40acf731
atiqbitstream <atiq.bitstream@gmail.com>
2025-04-14 11:29:00 +0500

    Integrated `ERole` enum in `Rider` interface to standardize role assignment

--- mooToYouFront/src/app/features/rider/interfaces/rider.interface.ts
+++ mooToYouFront/src/app/features/rider/interfaces/rider.interface.ts
@@ -1,9 +1,12 @@
+import { ERole } from "../../shared/enums/roles.enum";
+
 export interface Rider {
     id: number;
     firstName: string;
     lastName: string;
     phoneNumber: string;
     address: string;
+    role:ERole;
     sector: string;
     street: string;
     cnicNumber: string;
