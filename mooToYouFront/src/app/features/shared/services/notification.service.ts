9f26a550fe973092a688a88b76baf7a50c878c8b
atiqbitstream <atiq.bitstream@gmail.com>
2025-04-15 16:40:06 +0500

    added new functions to notification service

--- mooToYouFront/src/app/features/shared/services/notification.service.ts
+++ mooToYouFront/src/app/features/shared/services/notification.service.ts
@@ -7,10 +7,18 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 export class NotificationService {
   constructor(private snackBar: MatSnackBar) {}
 
-  show(message: string, action = 'close', duration = 3000) {
+  show(message: string, action = 'close', duration = 3000, panelClass:string[]=[]) {
     this.snackBar.open(message, action, {
       duration,
       verticalPosition: 'top',
     });
+  }
+
+  showSuccess(message:string):void{
+    this.show(message,'close',3000,['success-snackbar'])
+  }
+
+  showError(message:string):void{
+    this.show(message,'close',3000, ['error-snackbar'])
   }
 }
